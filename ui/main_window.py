d5fcc28e5f36a35026510dd908feb93be13facf6
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-07-02 18:17:59 -0600

    folders creation

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,8 +1,21 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PyQt5.QtCore import Qt, QSize
 import sys
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Main Window")
+        self.setFixedSize((QSize(100, 600)))
+
+        # Create a central widget
+        button = QPushButton("Click Me")
+        self.setCentralWidget(button)
+        
+
+
 app = QApplication(sys.argv)
-window = QWidget()
+window = MainWindow()
 window.show()
 
 app.exec_()
