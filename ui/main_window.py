f3c2e0a8eb7cc052fedf31dd26b89e2032f42b09
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-07-03 19:54:46 -0600

    Container and title changed

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,17 +1,37 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt, QSize
+from random import choice
 import sys
+
+windows_titles = [
+    "Welcome to the App",
+    "Main Application Window",
+    "User Interface",
+    "Application Dashboard",
+    "Interactive Window",
+    "Something went wrong"
+    ]
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Main Window")
-        self.setFixedSize((QSize(100, 600)))
 
-        # Create a central widget
-        button = QPushButton("Click Me")
-        self.setCentralWidget(button)
+        self.label = QLabel()
         
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
 
 
 app = QApplication(sys.argv)
