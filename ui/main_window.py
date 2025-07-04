db19fd28573dbd5729245546e54fbb0bca483386
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-07-04 17:22:24 -0600

    Checkbox

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,64 +1,39 @@
 import sys
 
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (
     QApplication,
     QCheckBox,
     QComboBox,
-    QDateEdit,
-    QDateTimeEdit,
-    QDial,
     QDoubleSpinBox,
-    QFontComboBox,
     QLabel,
-    QLCDNumber,
     QLineEdit,
+    QListWidget,
     QMainWindow,
-    QProgressBar,
-    QPushButton,
-    QRadioButton,
     QSlider,
     QSpinBox,
-    QTimeEdit,
-    QVBoxLayout,
-    QWidget,
 )
 
-# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("My App")
 
-        layout = QVBoxLayout()
-        widgets = [
-            QCheckBox,
-            QComboBox,
-            QDateEdit,
-            QDateTimeEdit,
-            QDial,
-            QDoubleSpinBox,
-            QFontComboBox,
-            QLCDNumber,
-            QLabel,
-            QLineEdit,
-            QProgressBar,
-            QPushButton,
-            QRadioButton,
-            QSlider,
-            QSpinBox,
-            QTimeEdit,
-        ]
+        widget = QCheckBox()
+        widget.setCheckState(Qt.Checked)
 
-        for w in widgets:
-            layout.addWidget(w())
+        # For tristate: widget.setCheckState(Qt.PartiallyChecked)
+        # Or: widget.setTriState(True)
+        widget.stateChanged.connect(self.show_state)
 
-        widget = QWidget()
-        widget.setLayout(layout)
-
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
         self.setCentralWidget(widget)
+
+    def show_state(self, s):
+        print(s == Qt.Checked)
+        print(s)
 
 app = QApplication(sys.argv)
 window = MainWindow()
