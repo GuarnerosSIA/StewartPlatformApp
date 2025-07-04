7ede956796d115e100885cfad01e5e75d8ae54b2
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-07-04 16:10:32 -0600

    Mouse Events

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,36 +1,27 @@
-from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QTextEdit
+from PyQt5.QtCore import Qt
 from random import choice
 import sys
 
-windows_titles = [
-    "Welcome to the App",
-    "Main Application Window",
-    "User Interface",
-    "Application Dashboard",
-    "Interactive Window",
-    "Something went wrong"
-    ]
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Main Window")
+        self.setMouseTracking(True)
+        self.label = QLabel("Click this window")
+        self.label.setMouseTracking(True)
+        self.setCentralWidget(self.label)
 
-        self.label = QLabel()
-        
-        self.input = QLineEdit()
-        self.input.textChanged.connect(self.label.setText)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.input)
-        layout.addWidget(self.label)
-
-        container = QWidget()
-        container.setLayout(layout)
-
-        self.setCentralWidget(container)
+    def mouseMoveEvent(self,e):
+        self.label.setText("Mouse moved")
+    def moursePressEvent(self, e):
+        self.label.setText("Mouse pressed")
+    def mouseReleaseEvent(self, e):
+        self.label.setText("Mouse released")
+    def mouseDoubleClickEvent(self, e):
+        self.label.setText("Mouse double clicked")
 
 
 
@@ -38,4 +29,4 @@ app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 
-app.exec_()
+app.exec()
